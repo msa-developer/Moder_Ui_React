2d68076de10e778f82dcf8221a12b6d44969cae9
msa-developer <shoaib9542186823@gmail.com>
2025-11-03 22:33:30 +0530

    initial commit before starting the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Nav from "./Components/Nav.jsx";
+import Hero from "./Components/Hero.jsx";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white overflow-hidden">
+      <Nav />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
