7ee1dd2c94ec03be24e8d7b95841e6705fd0367a
msa-developer <shoaib9542186823@gmail.com>
2026-04-26 16:24:07 +0530

    starting of Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Nav from "./Components/Nav.jsx";
-import Hero from "./Components/Hero.jsx";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-slate-900 text-white overflow-hidden">
-      <Nav />
-      <Hero />
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar />
     </div>
   );
 };
--- src/Components/Nav.jsx
+++
@@ -1,9 +0,0 @@
-const Nav = () => {
-  return (
-    <nav className="fixed w-full top-0 z-50 transition-all duration-300 bg-slate-950/50 backdrop-blur-sm">
-      <div className="max-w-7xl bg-black mx-auto px-4 lg:px-8">hello</div>
-    </nav>
-  );
-};
-
-export default Nav;
