782889e6e99087ac6c1aa57173facc0166c8cdc7
msa-developer <shoaib9542186823@gmail.com>
2026-04-27 12:47:17 +0530

    started building the coding area part

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,9 +20,12 @@ const Hero = () => {
       <div className="absolute top-20 left-4 sm:left-10 w-48 sm:w-72 h-48 sm:h-72 bg-blue-500/10 rounded-full blur-3xl animate-pulse" />
       <div className="absolute bottom-20 right-4 sm:right-10 w-64 sm:w-96 h-64 sm:h-96 bg-cyan-500/10 rounded-full blur-3xl animate-pulse delay-1000" />
 
-      <div>
-        <div>
-          <div></div>
+      <div className="relative order-2 w-full">
+        <div className="relative bg-white/5 backdrop-blur-xl rounded-xl sm:rounded-2xl p-3 sm:p-4 shadow-2xl border border-white/10">
+          <div className="bg-gradient-to-br from-gray-900/20 to-gray-800/20 backdrop-blur-sm rounded-lg overflow-hidden h-[280px] sm:w-[350px] lg:h-[450px] border-white/5">
+            {/*Header IDE*/}
+            <header className="flex items-center justify-between px-3 sm:px-4 py-2 sm:py-3 bg-white/5 backdrop-blur-sm border-b border-white/10"></header>
+          </div>
         </div>
       </div>
     </section>
