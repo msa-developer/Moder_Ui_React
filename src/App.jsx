d46d38ca4baf32e0662bd0838ef3f955ddec58d7
msa-developer <shoaib9542186823@gmail.com>
2026-04-27 07:34:50 +0530

    did part upto showing menu list in mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-slate-900 text-white overflow-hidden">
+    <div className="min-h-screen overflow-hidden bg-slate-900 text-white">
       <Navbar />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,63 @@
+import { Menu, X } from "lucide-react";
+import React from "react";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
   return (
-    <nav className="transition-all w-full duration-300 bg-red-800/20 backdrop-blur-sm fixed top-0 z-50 ">
-      <div className="h-14 sm:h-20 max-w-7xl mx-auto flex items-center">
-        <div className="flex justify-around items-center">
-          {/*Logo */}
-          <div className="flex space-x-1 px-2">
-            <div>
-              <img
-                src="./favicon.svg"
-                alt="favicon"
-                className="h-6 w-6 sm:h-10 sm:w-10"
-              />
-            </div>
-            <span className="font-bold sm:text-2xl">
-              <span>Modern</span>
+    <nav className="transition-all duration-300 w-full bg-red-900/50 backdrop-blur-sm px-4">
+      <main className="h-14 sm:h-20 max-w-7xl flex items-center justify-between px-4 sm:px-10 lg:px-12 mx-auto">
+        <section className="flex justify-between items-center">
+          {/*Logo*/}
+          <div className="group cursor-pointer flex items-center space-x-1">
+            <img
+              src="./favicon.svg"
+              alt="favicon"
+              className="w-8 h-8 sm:w-10 sm:h-10"
+            />
+            <span className="text-bold sm:text-xl lg:text-2xl">
+              <span className="text-white">Modern</span>
               <span className="text-blue-400">ReactUi</span>
             </span>
           </div>
+        </section>
+        {/*Links*/}
+        <section className="hidden sm:flex space-x-2">
+          <a href="#features" className="text-gray-300 hover:text-white">
+            Features
+          </a>
+          <a href="#testimonials" className="text-gray-300 hover:text-white">
+            Testimonials
+          </a>
+          <a href="#AboutUs" className="text-gray-300 hover:text-white">
+            AboutUs
+          </a>
+        </section>
+        {/*button*/}
+        <button
+          className="sm:hidden text-gray-300 hover:text-white"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
+          {isOpen ? <X /> : <Menu />}
+        </button>
+      </main>
 
-          {/*Links*/}
-        </div>
-      </div>
+      {/*Show Menu */}
+      {isOpen ? (
+        <section className="slide-from-top duration-300 animate-both">
+          <a href="#features" className="text-gray-300 hover:text-white block">
+            Features
+          </a>
+          <a
+            href="#testimonials"
+            className="text-gray-300 hover:text-white block"
+          >
+            Testimonials
+          </a>
+          <a href="#AboutUs" className="text-gray-300 hover:text-white block">
+            AboutUs
+          </a>
+        </section>
+      ) : null}
     </nav>
   );
 };
